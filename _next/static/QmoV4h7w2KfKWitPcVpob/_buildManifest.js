5d618efaa96fb58ee596c38dac1ef58f7014799e
HeroSony <simvirak7@gmail.com>
2020-12-05 16:29:10 +0000

    Deploying to gh-pages from  @ fc538f8418729280cf0691cc8959800ddcbdac9c 🚀

--- _next/static/QmoV4h7w2KfKWitPcVpob/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-1ff1c518146ce67cfb97.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-7593f4a919f64ae47c46.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/SgFxaN-KFx3mqmNsI6va4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-1ff1c518146ce67cfb97.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-62ef564fcc0860a3090f.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
